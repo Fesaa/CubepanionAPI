f194025baaa3b56a8c074aaae8356a3eeff5e076
Amelia <77553571+Fesaa@users.noreply.github.com>
2023-11-22 12:18:53 +0100

    Implement #4 (#6)

--- src/prometheus/mod.rs
+++ src/prometheus/mod.rs
@@ -1,4 +1,4 @@
-use actix_web::{Responder, get, HttpResponse};
+use actix_web::{get, HttpResponse, Responder};
 use metrics::{register_counter, register_histogram};
 use metrics_prometheus::Recorder;
 
@@ -7,6 +7,7 @@ pub async fn setup_metrics() -> Result<Recorder, Box<dyn std::error::Error>> {
 
     register_counter!("total_requests", "endpoint" => "");
     register_counter!("success_requests", "endpoint" => "");
+    register_counter!("lb-submissions", "game" => "");
     register_histogram!("request_duration", "endpoint" => "");
 
     return Ok(recorder);
@@ -14,10 +15,13 @@ pub async fn setup_metrics() -> Result<Recorder, Box<dyn std::error::Error>> {
 
 #[get("/prometheus")]
 pub async fn get_metrics() -> impl Responder {
-    let report = prometheus::TextEncoder::new()
-        .encode_to_string(&prometheus::default_registry().gather());
+    let report =
+        prometheus::TextEncoder::new().encode_to_string(&prometheus::default_registry().gather());
     return match report {
         Ok(s) => HttpResponse::Ok().body(s),
-        Err(e) => HttpResponse::InternalServerError().body(format!("Error while encoding metrics, {}", e)),
+        Err(e) => {
+            HttpResponse::InternalServerError().body(format!("Error while encoding metrics, {}", e))
+        }
     };
 }
+
