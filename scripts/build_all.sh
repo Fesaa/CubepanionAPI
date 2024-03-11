69968b2517806ff870c4de36ce51ecadfa98ee87
Fesaa <77553571+Fesaa@users.noreply.github.com>
2024-03-12 00:32:55 +0100

    Docker better dependancy management
    Stops go mod download from renunning every time even tho nothing changed there

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-docker build -t chests-service . -f Dockerfile.chest-service &
+docker build -t cubepanion-api-base . -f docker/Dockerfile.base
+
+docker build -t chests-service . -f docker/Dockerfile.chest-service &
 pids+=($!)
 
-docker build -t cubesocket . -f Dockerfile.cubesocket &
+docker build -t cubesocket . -f docker/Dockerfile.cubesocket &
 pids+=($!)
 
-docker build -t games-service . -f Dockerfile.games-service &
+docker build -t games-service . -f docker/Dockerfile.games-service &
 pids+=($!)
 
-docker build -t leaderboard-service . -f Dockerfile.leaderboard-service &
+docker build -t leaderboard-service . -f docker/Dockerfile.leaderboard-service &
 pids+=($!)
 
-docker build -t maps-service . -f Dockerfile.maps-service &
+docker build -t maps-service . -f docker/Dockerfile.maps-service &
 pids+=($!)
 
 # Wait for all background processes and check for errors
