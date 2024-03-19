92563d06a978796b2026d5a21f4ba67747195371
Fesaa <77553571+Fesaa@users.noreply.github.com>
2024-03-19 11:05:55 +0100

    Use Get to have it cached

--- leaderboard-service/main.go
+++ leaderboard-service/main.go
@@ -28,7 +28,7 @@ func main() {
 	ms.Get("/player/:name", PlayerLeaderboard)
 	ms.Get("/game/:game", GameLeaderboard)
 	ms.Get("/game/:game/bounded", GameLeaderboardBounded)
-	ms.Post("/batch", BatchPlayerLeaderboard)
+	ms.Get("/batch", BatchPlayerLeaderboard)
 
 	err = ms.Start()
 	if err != nil {
