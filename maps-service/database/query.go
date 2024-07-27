3367cea66cc9977b973052744aee74e7d78701fe
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-07-27 15:30:20 +0200

    Don't * select

--- maps-service/database/query.go
+++ maps-service/database/query.go
@@ -11,12 +11,12 @@ var getGenerators *sql.Stmt
 func load(db *sql.DB) error {
 	var err error
 
-	getGameMaps, err = db.Prepare("SELECT * FROM game_maps")
+	getGameMaps, err = db.Prepare("SELECT game,unique_name,map_name,team_size,build_limit,colours,layout  FROM game_maps")
 	if err != nil {
 		return fmt.Errorf("Error preparing getGameMaps: %v", err)
 	}
 
-	getGenerators, err = db.Prepare("SELECT * FROM generators")
+	getGenerators, err = db.Prepare("SELECT unique_name,ordering,gen_type,gen_location,level,count FROM generators")
 	if err != nil {
 		return fmt.Errorf("Error preparing getGenerators: %v", err)
 	}
